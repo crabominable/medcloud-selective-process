8c2e6fb893391fb7b3b16ca5a99ec52afbe67662
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-08-14 02:13:25 -0300

    fix: fixing start.sh v2: comments conflict

--- pm2/start.sh
+++ pm2/start.sh
@@ -1,16 +1,3 @@
-#!/usr/bin/env bash
-
-# echo 'Checking up MySQL...'
-# until mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -e ";" ; do
-#    echo 'Waiting MySQL...'
-#     sleep 5
-# done
-
-# DIR="/app/www/api/dist"
-# if [[ ! -e $DIR ]]; then
-#   cd /app/www/api/ && npm run rebuild
-# fi
-
 cd /app/medcloud-selective-process/backend && npx prisma migrate
 
 if [ "$NODE_ENV" == "development" ]
@@ -20,5 +7,4 @@ else
   pm2 start /app/medcloud-selective-process/pm2/pm2-production.json
 fi
 
-# Keep Container Running
 tail -f /dev/null
