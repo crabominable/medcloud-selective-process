d11a63aea1c2eca4e2881d7436dacd9a2fea0d38
Lucas Pinheiro <lucaspinheiroalberdanha@gmail.com>
2022-08-14 02:39:53 -0300

    fix: start.sh v3

--- pm2/start.sh
+++ pm2/start.sh
@@ -1,10 +1,8 @@
 cd /app/medcloud-selective-process/backend && npx prisma migrate
-
 if [ "$NODE_ENV" == "development" ]
 then
   pm2 start /app/medcloud-selective-process/pm2/pm2-development.json
 else
   pm2 start /app/medcloud-selective-process/pm2/pm2-production.json
 fi
-
 tail -f /dev/null
